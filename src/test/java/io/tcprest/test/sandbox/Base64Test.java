003ba1a097365343295493e08a89e60858ef045f
Weinan Li <l.weinan@gmail.com>
2012-08-20 01:52:00 +0800

    add missing files

---
+++ src/test/java/io/tcprest/test/sandbox/Base64Test.java
@@ -0,0 +1,26 @@
+package io.tcprest.test.sandbox;
+
+import io.tcprest.commons.Base64;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * http://www.mkyong.com/java/how-do-convert-byte-array-to-string-in-java/
+ * @author Weinan Li
+ * @created_at 08 20 2012
+ */
+public class Base64Test {
+
+    @Test
+    @Ignore
+    public void testEncodeAndDecode() {
+        String encodedStr = Base64.encode("abc".getBytes());
+        String decodedStr = new String(Base64.decode(encodedStr));
+        assertEquals("abc", decodedStr);
+
+        System.out.println(Base64.encode("Jack".getBytes()));
+        System.out.println(Base64.encode("Lucy".getBytes()));
+    }
+}
