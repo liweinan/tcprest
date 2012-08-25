a5b0ecf30aecce139206b6070b35bd12702d8537
Weinan Li <l.weinan@gmail.com>
2012-08-26 00:09:17 +0800

    minor fixes

--- src/test/java/io/tcprest/test/smoke/TcpClientFactorySmokeTest.java
+++ src/test/java/io/tcprest/test/smoke/TcpClientFactorySmokeTest.java
@@ -51,8 +51,6 @@ public class TcpClientFactorySmokeTest {
 
         assertEquals("Hello, world!", client.helloWorld());
         assertEquals("a,2,true123.0111", client.allTypes("a", 2, true, (short) 1, 2L, 3.0, (byte) 'o'));
-
-//        assertEquals("Hello, 张三", client.sayHelloTo("张三"));
     }
 
     @Test(expected = Exception.class)
@@ -145,7 +143,5 @@ public class TcpClientFactorySmokeTest {
         for (int i = 0; i < in.length; i++) {
             assertEquals(in[i], out[i]);
         }
-
-
     }
 }
